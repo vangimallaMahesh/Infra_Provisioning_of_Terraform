9326d9842065c8bdeddd4350694f7e25621edd24
vangimallaMahesh <105789814+vangimallaMahesh@users.noreply.github.com>
2023-04-25 17:25:16 +0530

    Update main.tf

--- AWS/main.tf
+++ AWS/main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "ec2-user" {
   ami           = "ami-06fc49795bc410a0c" # us-west-2
   instance_type = "t2.micro"
   tags = {
-      Name = "Terraform server 2"
+      Name = "Server 1"
   }
 }
